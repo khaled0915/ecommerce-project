19ff6529266815a8a34c45b0d0ca1237c50fe2d1
khaled0195 <khaledsaifullah50956@gmail.com>
2025-07-18 01:36:13 +0600

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 
+import { Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home'
+
+import About from './pages/About'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Collection from './pages/collection'
+import Contact from './pages/contact'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+
+
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
-      <h1 className='text-3xl font-bold underline text-amber-500'> Hello </h1>
-      
+       <Navbar />
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product' element={<Product />} />
+
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders/>} />
+
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Collection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Collection;
+
+
+
